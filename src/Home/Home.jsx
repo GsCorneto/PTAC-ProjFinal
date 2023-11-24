1290da6a713190a706b10f2c5f69f6c1d6f6f8a0
GsCorneto <131817217+GsCorneto@users.noreply.github.com>
2023-11-24 16:45:00 -0400

    Initial commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import './home.css' 
+
+
+export default function Home(){
+    return(
+        <div>
+        <header> 
+         <h1>Torneio Mundial</h1>
+        </header>
+
+        <nav>
+    <ul>
+       <li>Torneios as 16h</li>
+       <li>Sorvete de creme à 20 passos da entrada</li>
+       <li>Carnes á direita da saída</li>
+       <li>NÃO TEMOS SEGURO CONTRA MORTES ;)</li>      
+   </ul>
+</nav>
+
+           <main>
+             <Link to="/todo">Adicionar campeão</Link>
+           </main>
+
+    <footer></footer>
+     </div>
+     );
+}
